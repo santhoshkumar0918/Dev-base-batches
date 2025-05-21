c3ff22845ed11bf01229bbc822a0c81be2770f0c
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-05-21 13:45:58 +0530

    added

--- contracts/interfaces/IRegistry.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-// This is a blank interface file that was referenced in errors.
-// You may need to define the actual interface content based on your requirements.
-interface IRegistry {
-    // Add required interface methods here
-}
