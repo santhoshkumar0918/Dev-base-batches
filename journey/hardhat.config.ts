d26296b3bcb053d7f9e9f1481ef5ce7f9e2938b7
MohamedAshiq09 <mohamedashiq782@gmail.com>
2025-05-28 11:57:53 +0530

    changed the hardhat confiig file to sonic

--- journey/hardhat.config.ts
+++ journey/hardhat.config.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 
 // Load environment variables
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const POLYGON_AMOY_RPC_URL =
-  process.env.POLYGON_AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/";
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "";
+const SONIC_BLAZE_RPC_URL = 
+  process.env.SONIC_BLAZE_RPC_URL || "https://rpc.blaze.soniclabs.com";
+const SONIC_API_KEY = process.env.SONIC_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -22,25 +22,25 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {},
-    polygonAmoy: {
-      url: POLYGON_AMOY_RPC_URL,
+    sonicBlaze: {
+      url: SONIC_BLAZE_RPC_URL,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      chainId: 80002,
+      chainId: 57054,
       gasPrice: "auto",
       timeout: 60000, // 60 seconds
     },
   },
   etherscan: {
     apiKey: {
-      polygonAmoy: POLYGONSCAN_API_KEY,
+      sonicBlaze: SONIC_API_KEY,
     },
     customChains: [
       {
-        network: "polygonAmoy",
-        chainId: 80002,
+        network: "sonicBlaze",
+        chainId: 57054,
         urls: {
-          apiURL: "https://api-amoy.polygonscan.com/api",
-          browserURL: "https://amoy.polygonscan.com/",
+          apiURL: "https://testnet.sonicscan.org/api",
+          browserURL: "https://testnet.sonicscan.org/",
         },
       },
     ],
